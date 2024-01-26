8597b73462db1ef0414b62eee09a78c7d128a226
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-01-26 20:25:00 +0100

    completed the algorithm exercises

--- js/js-algorithm.js
+++ js/js-algorithm.js
@@ -2,20 +2,20 @@ const numbers = [2, 3, 4, 5, 3, 2];
 const emptyArr = [2];
 
 // Length check function
-// const len = (array) => {
-//   console.log(array.length);
-// };
+const len = (array) => {
+  console.log(array.length);
+};
 
-// len(numbers);
+len(numbers);
 
-//  Get the last element of an array without using .length
-// const last = (array) => {
-//   // check if array is empty - Another way: Array.isArray(names) && names.length === 0;
-//   if (array != []) console.log("This is an empty array");
-//   console.log(array.slice(-1));
-// };
+// Get the last element of an array without using .length
+const last = (array) => {
+  // check if array is empty - Another way: Array.isArray(names) && names.length === 0;
+  if (array != []) console.log("This is an empty array");
+  console.log(array.slice(-1));
+};
 
-// last(emptyArr);
+last(emptyArr);
 
 // but last function
 const butLast = (array) => {
@@ -25,29 +25,78 @@ const butLast = (array) => {
 
 butLast(emptyArr);
 
-// // Create a reverse copy
-// const reverse = (array) => {
-//   reverseArr = array.slice().reverse();
-//   console.log(reverseArr);
-// };
+// Create a reverse copy
+const reverse = (array) => {
+  reverseArr = array.slice().reverse();
+  console.log(reverseArr);
+};
 
-// reverse(numbers);
+reverse(numbers);
 
-// // Check if array is Palindrome function
-// const isPalindrome = (array) => {
-//   if (array[0] === array[array.length - 1]) {
-//     console.log(true);
-//   } else {
-//     console.log(false);
-//   }
-// };
-// isPalindrome(numbers);
+// Check if array is Palindrome function
+const isPalindrome = (array) => {
+  if (array[0] === array[array.length - 1]) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
+isPalindrome(numbers);
 
-// // filter duplicated
-// const compress = (array) => {
-//   newArr = new Set(array); //arrays with unique values
-//   console.log(newArr);
-// };
-// compress(numbers);
+// filter duplicated
+const compress = (array) => {
+  newArr = new Set(array); //arrays with unique values
+  console.log(newArr);
+};
+compress(numbers);
 
-// // pack duplicated elements in different arrays
+// pack duplicated elements in different arrays
+const arrayExample = [3, 3, 3, 2, 2, 5, 5, 5, 4, 1, 1, 1];
+
+const pack = (array) => {
+  packedArray = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    if (
+      packedArray.length > 0 &&
+      packedArray[packedArray.length - 1][0] === array[i]
+    ) {
+      packedArray[packedArray.length - 1].push(array[i]);
+      continue;
+    }
+    packedArray.push([array[i]]);
+  }
+  console.log(packedArray);
+  return packedArray;
+};
+
+// Create encode function to show duplicated data and its quantity
+const encode = (array) => {
+  const packedArrays = pack(array);
+  const encodeArr = [];
+  packedArrays.forEach((packedArray) => {
+    encodeArr.push([packedArray.length, packedArray[0]]);
+  });
+  return encodeArr;
+};
+
+const encode2a = (array) => {
+  const packedArrays = pack(array);
+  const encodeArr = [];
+  packedArrays.forEach((packedArray) => {
+    if (packedArray.length === 1) {
+      encodeArr.push(packedArray[0]);
+    } else {
+      encodeArr.push([packedArray.length, packedArray[0]]);
+    }
+  });
+  return encodeArr;
+};
+
+/*  for (let i=0 ; i < (array.length-1); i++) {
+   if (array[i] !== array[i+1] && array[i] !== array[i-1]) {
+     // Logic for handling single numbers.
+     continue;
+   }
+ } */
+
+console.log(encode2a(arrayExample));
