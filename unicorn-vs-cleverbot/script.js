d707631a9954cf5c77a4eb86e705e0f3478e5bb2
Nhi <nhipham0902@gmail.com>
2024-01-29 11:02:21 +0100

    created vite folder structure

--- unicorn-vs-cleverbot/script.js
+++ unicorn-vs-cleverbot/script.js
@@ -1,6 +1,8 @@
-import { conversation } from "./chat.js";
-// import {test as testFunction} from ".test/js"
+import { conversation } from "./src/data.js";
+import { appendMessage, displayWithDelay, chatScreen } from "./src/function.js";
+// import {test as testFunction, test2} from ".test/js"
 // testFunction();
+// import * from  ".test/js" - import all
 
 // const chatScreen = document.querySelector(".conversation");
 
@@ -21,35 +23,5 @@ import { conversation } from "./chat.js";
 //   message.appendChild(messageText);
 //   chatScreen.appendChild(message);
 // });
-
-const chatScreen = document.querySelector(".conversation");
-
-const appendMessage = (chat) => {
-  // chatScreen.className = `message message--${conversation_json.side};
-  const message = document.createElement("div");
-  message.classList.add("message");
-
-  const messageText = document.createElement("div");
-  messageText.className = "message__text";
-  messageText.innerText = `${chat.text}`;
-
-  if (chat.side === "local") {
-    message.classList.add("message--local");
-  } else if (chat.side === "remote") {
-    message.classList.add("message--remote");
-  }
-
-  message.appendChild(messageText);
-  chatScreen.appendChild(message);
-};
-
-const displayWithDelay = (messages, i) => {
-  if (i < messages.length) {
-    appendMessage(messages[i]);
-    setTimeout(() => {
-      displayWithDelay(messages, i + 1);
-    }, 500);
-  }
-};
 
 displayWithDelay(conversation, 0);
---
+++ unicorn-vs-cleverbot/src/function.js
@@ -0,0 +1,30 @@
+const chatScreen = document.querySelector(".conversation");
+const appendMessage = (chat) => {
+  // chatScreen.className = `message message--${conversation_json.side};
+  const message = document.createElement("div");
+  message.classList.add("message");
+
+  const messageText = document.createElement("div");
+  messageText.className = "message__text";
+  messageText.innerText = `${chat.text}`;
+
+  if (chat.side === "local") {
+    message.classList.add("message--local");
+  } else if (chat.side === "remote") {
+    message.classList.add("message--remote");
+  }
+
+  message.appendChild(messageText);
+  chatScreen.appendChild(message);
+};
+
+const displayWithDelay = (messages, i) => {
+  if (i < messages.length) {
+    appendMessage(messages[i]);
+    setTimeout(() => {
+      displayWithDelay(messages, i + 1);
+    }, 500);
+  }
+};
+
+export { appendMessage, displayWithDelay, chatScreen };
